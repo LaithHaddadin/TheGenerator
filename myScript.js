2d4e4e20705456bebec3f13fcd6de2e59c204432
Laith <lhaddadin.25@acsamman.edu.jo>
2022-06-02 16:28:56 +0300

    correcting code

--- myScript.js
+++ myScript.js
@@ -7,13 +7,32 @@ function randomFirstName(){
 	//create multiple arrays of name parts
 	//select several random name parts
 
-	let wordBeginning = ["Ac", "Rn", "Bl"];
-	let beginning = wordBeginning[Math.floor(Math.random() * wordBeginning.length)];
 
-	let wordEnd = ["rd", "ft", "ue"];
-	let end = wordEnd[Math.floor(Math.random() * wordEnd.length)];
+	
+	let wordletterA = ["Alexander", "Asher", "Ann", "Andrew", "Abigale"];
+	let letterA = wordletterA[Math.floor(Math.random() * wordletterA.length)];
+
+
+
+	let wordletterB = ["Bella", "Ben", "Bethany", "Brody", "Bruce"];
+	let letterB = wordletterB[Math.floor(Math.random() * wordletterB.length)];
+
+
+
+	let wordletterC = ["Carter", "Carson", "Chase", "Charlotte", "Carol", "Caroline", "Clara"];
+	let letterC = wordletterC[Math.floor(Math.random() * wordletterC.length)];
+
+
+
+	let wordletterM = ["Mia", "Madelyn", "Maria", "Mackenzie", "Miles", "Matteo", "Max", "Maddox", "Mathew"];
+	let letterM = wordletterM[Math.floor(Math.random() * wordletterM.length)];
+
 
 	//combine all name parts into a single string
-	name = beginning + end;
+
+	let firstname =[["Alexander", "Asher", "Ann", "Andrew", "Abigale"], ["Bella", "Ben", "Bethany", "Brody", "Bruce"], ["Carter", "Carson", "Chase", "Charlotte", "Carol", "Caroline", "Clara"], ["Mia", "Madelyn", "Maria", "Mackenzie", "Miles", "Matteo", "Max", "Maddox", "Mathew"]];
+	let name = firstname[Math.floor(Math.random() * firstname.length)]   [[Math.floor(Math.random() * firstname.length)]];
+	
+	//arr[i][j]
 	return name;
 } 
